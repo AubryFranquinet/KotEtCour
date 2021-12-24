3516ed1785f8cc57cfd4b8cafa90ece7471cebc1
Aubry Franquinet <aubry.franquinet@hotmail.com>
2021-12-24 13:53:26 +0100

    Started front-end with homepage header

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header").style.fontSize = "30px";
+    } else {
+        document.getElementById("header").style.fontSize = "90px";
+    }
+}
